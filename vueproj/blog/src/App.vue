3b18f147bcf835ede40a6e0b1589a31fff563637
xile <1223162589@qq.com>
2021-09-21 00:41:12 +0800

    文章显示，搜索功能

--- vueproj/blog/src/App.vue
+++ vueproj/blog/src/App.vue
@@ -1,16 +1,33 @@
 <template>
   <div id="app">
    <addblog></addblog>
+   <showBlogs></showBlogs>
+   <!-- <h2 @click.per="addblog">博客</h2>
+   <h2 @click="blog">写博客</h2> -->
   </div>
 </template>
 
 <script>
 import addblog from './components/addblog'
+import showBlogs from './components/showBlogs'
 export default {
   name: 'App',
   components:{
     addblog,
-  }
+    showBlogs,
+  },
+  methods: {
+    addblog(){
+      this.$router.push({
+        path:'/addblog'
+      });
+    },
+    blog(){
+      this.$router.push({
+        path:'/showBlogs'
+      });
+    }
+  },
 }
 </script>
 
